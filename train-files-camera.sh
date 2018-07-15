4132d9a3449bd7323c773f6614688d0df002793c
Arne Peine <arnepeine@gmail.com>
2018-07-15 19:06:55 +0200

    Update train-files-camera.sh

--- train-files-camera.sh
+++ train-files-camera.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 #This script collects pictures from the Raspberry Pi camera and puts them into folders for NN training
-
-echo "Welcome to the training image acquisition tool for Consumabot"
+echo "Welcome to the training tool for Consumabot"
 echo "This script is released under GPL 3.0 license"
+echo "Please select your desired action:"
+echo "----------------------------------"
+echo "1 - Train bot for new items"
+echo "2 - Sync with training server"
+echo "3 - Train models and sync back"
+echo "What's your choice?"
+read choice
+
+if [ "$choice" == "1" ]
+then
+
+
 
 echo "How long do we want to train? (in seconds)"
 read length
@@ -26,3 +37,8 @@ longms=$(($length*1000))
 intervalms=$(($interval*1000))
 raspistill -t $longms -tl $intervalms -o ~/train/$prefix/%d.jpg -v -awb auto -sh 100
 sleep 10
+
+else
+echo "Synchronizing training folders"
+rsync -avz -e 'ssh' ~/train/ root@94.16.119.125:~/train/ 
+fi
